db93540fe1aa131a6399d6e9da7c59a82588fd96
Davide Ginnasio <d.ginnasio@tdnet.it>
2026-01-20 21:09:46 +0100

    firebase var

---
+++ firebase-var.js
@@ -0,0 +1,11 @@
+// Firebase Configuration Variables
+// Questo file contiene le credenziali Firebase (non committare!)
+
+const JS_FIREBASE_API_KEY = "AIzaSyAqVCf232iMEixXNDZkxUL5RFVEU-BRsx4";
+const JS_FIREBASE_AUTH_DOMAIN = "bais-game.firebaseapp.com";
+const JS_FIREBASE_DATABASE_URL = "https://bais-game-default-rtdb.europe-west1.firebasedatabase.app";
+const JS_FIREBASE_PROJECT_ID = "bais-game";
+const JS_FIREBASE_STORAGE_BUCKET = "bais-game.firebasestorage.app";
+const JS_FIREBASE_MESSAGING_SENDER_ID = "157534285184";
+const JS_FIREBASE_APP_ID = "1:157534285184:web:e00305b971ed231f7f6565";
+const JS_FIREBASE_MEASUREMENT_ID = "G-S62QQXBX3S";
